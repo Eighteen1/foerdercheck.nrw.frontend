b7c840ed3f7f66ec16174643187221085646acce
Raphael Pappalardo <c.mrg.studios@gmail.com>
2025-04-16 16:08:14 +0900

    backend link fix

--- src/components/PersonalSpace.tsx
+++ src/components/PersonalSpace.tsx
@@ -35,7 +35,7 @@ const PersonalSpace: React.FC = () => {
     e.preventDefault();
     try {
       // Create user through backend
-      const createUserResponse = await fetch('https://foerdercheck-backend.onrender.com/user/create', {
+      const createUserResponse = await fetch('https://foerdercheck-backend.onrender.com/api/user/create', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
